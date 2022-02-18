1d18c860db9ffedcdb4d8536fc0dac28bd3b2f88
peri <parizat@live.com>
2022-02-18 09:03:39 -0800

    task6

--- src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/DropdownPractice.java
+++ src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/DropdownPractice.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -52,15 +53,37 @@ public class DropdownPractice {
     @Test
     public void dropdown_task6(){
         //Select “December 1 , 1923” and verify it is selected.
-        Select yearDropdown = new Select(driver.findElement(By.xpath("")));
-        Select monthDropdown = new Select(driver.findElement(By.xpath("")));
-        Select dayDropdown = new Select(driver.findElement(By.xpath("")));
-
+        Select yearDropdown = new Select(driver.findElement(By.xpath("//select[@id='year']")));
+        Select monthDropdown = new Select(driver.findElement(By.xpath("select[@id='month']")));
+        Select dayDropdown = new Select(driver.findElement(By.xpath("select[@id='day']")));
 
         //Select year using : visible text
+        yearDropdown.selectByVisibleText("1923");
+
         //Select month using : value attribute
+        monthDropdown.selectByValue("11");
+
         //Select day using : index number
+        dayDropdown.selectByIndex(0);
 
+        //creating expected values
+        String expectedYear = "1923";
+        String expectedMonth = "December";
+        String expectedDay = "1";
 
+        //getting actual values from browser
+        String actualYear = yearDropdown.getFirstSelectedOption().getText();
+        String actualMonth = monthDropdown.getFirstSelectedOption().getText();
+        String actualDay = dayDropdown.getFirstSelectedOption().getText();
+
+        //create assertions
+        Assert.assertTrue(actualYear.equals(expectedYear));
+        Assert.assertEquals(actualMonth, expectedMonth);
+        Assert.assertEquals(actualDay, expectedDay);
+
+    }
+    @AfterMethod
+    public void teardownMethod(){
+        driver.close();
     }
 }
