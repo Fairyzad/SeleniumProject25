42ce42af7f13ba13172362b3e521568647b80ad6
peri <parizat@live.com>
2022-02-18 13:56:21 -0800

    task6

--- src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/T4_IframePractice.java
+++ src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/T4_IframePractice.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
@@ -27,16 +28,33 @@ public class T4_IframePractice {
         //2. Go to website:https://practice.cydeo.com/iframe
         driver.get("https://practice.cydeo.com/iframe");
     }
+   // @Ignore
     @Test
     public void iframe_test(){
 
         // We need to switch driver's focus to iframe
         // option #1 -switching to iframe using id attribute value
-        driver.switchTo().frame("mce_0_ifr");
+       // driver.switchTo().frame("mce_0_ifr");
+
+        // option#2 - passing index number of iframe
+        driver.switchTo().frame(0);
+
+        //option#3 - locate as web element and pass in frame()method
+        driver.switchTo().frame(driver.findElement(By.xpath("//iframe[@id='mce_0_ifr']")));
 
         // locate the p tag
         WebElement yourContentGoesHereText = driver.findElement(By.xpath("//p")); // driver is foucing on the root html
 
         Assert.assertTrue(yourContentGoesHereText.isDisplayed());
+
+        //Verify"An iFrame contaning the TingMCM WYSIWYG Editor";
+        // To be able to verify the header, we must switch back to "main HTML"
+        driver.switchTo().parentFrame();
+
+        WebElement headerText = driver.findElement(By.xpath("//h3"));
+
+        //assertion of header text is displayed or not
+        Assert.assertTrue(headerText.isDisplayed());
+
     }
 }
