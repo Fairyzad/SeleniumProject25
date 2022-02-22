904a032ad97f96edb782da3e758805a5aac5a81a
peri <parizat@live.com>
2022-02-22 08:44:16 -0800

    T1_WindowHandling

--- src/test/java/com/cydeo/tests/day7_webtables_utilities_javafaker/T1_WindowHandling.java
+++ src/test/java/com/cydeo/tests/day7_webtables_utilities_javafaker/T1_WindowHandling.java
@@ -3,6 +3,7 @@ package com.cydeo.tests.day7_webtables_utilities_javafaker;
 import com.cydeo.utilities.WebDriverFactory;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -43,6 +44,10 @@ public class T1_WindowHandling {
 
         }
         //5. Assert:Title contains “Etsy”
+        String actualTitle = driver.getTitle();
+        String expectedInTitle="Etsy";
+
+        Assert.assertTrue(actualTitle.contains(expectedInTitle));
         //Lines to be pasted:
         // ((JavascriptExecutor) driver).executeScript("window.open('http://google.com','_blank');");
         // ((JavascriptExecutor) driver).executeScript("window.open('http://etsy.com','_blank');");
