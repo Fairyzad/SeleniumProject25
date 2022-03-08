18c0ee4bf50b9d734cbc20d152070868c6da193f
peri <parizat@live.com>
2022-03-08 10:35:55 -0800

    day9 javaFaker

--- src/test/java/com/cydeo/tests/day9_javafaker_testbase_driverUtil/JavaFakerPractice.java
+++ src/test/java/com/cydeo/tests/day9_javafaker_testbase_driverUtil/JavaFakerPractice.java
@@ -1,8 +1,19 @@
 package com.cydeo.tests.day9_javafaker_testbase_driverUtil;
 
+import com.github.javafaker.Faker;
 import org.testng.annotations.Test;
 
 public class JavaFakerPractice {
 
+@Test
+    public void test1(){
+    // creating faker object to reach methods
+    Faker faker =new Faker();
 
+    System.out.println("faker.name().firstName() = " + faker.name().firstName());
+    System.out.println("faker.name().lastName() = " + faker.name().lastName());
+
+    System.out.println("faker.name().fullName() = " + faker.name().fullName());
+
+}
 }
