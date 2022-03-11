751c9419af6f5f9279aa29d0de705a88b8e9ab69
peri <parizat@live.com>
2022-03-11 13:13:31 -0800

    day11_ActionsPractices

--- src/test/java/com/cydeo/tests/day11_actions_jsexcutor_pratice/ActionsPractices.java
+++ src/test/java/com/cydeo/tests/day11_actions_jsexcutor_pratice/ActionsPractices.java
@@ -31,6 +31,24 @@ public class ActionsPractices {
 //        2- Scroll back up to “Home” link using PageUP button
         actions.sendKeys(Keys.PAGE_UP,Keys.PAGE_UP).perform();
 
+       Driver.getDriver().close();
+    }
+
+    @Test
+    public void test2(){
+        Driver.getDriver().get("https://practice.cydeo.com/ ");
+        Driver.closeDriver();
+    }
+    @Test
+    public void test3(){
+        Driver.getDriver().get("https://practice.cydeo.com/ ");
+        Driver.closeDriver();
+
+    }
+    @Test
+    public void test4(){
+        Driver.getDriver().get("https://practice.cydeo.com/ ");
+        Driver.closeDriver();
 
     }
 }
