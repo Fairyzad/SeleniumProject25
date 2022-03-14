8147034eee4000cb1ea793d5209e189d11c5abf2
peri <parizat@live.com>
2022-03-14 08:37:42 -0700

    day12.Page

--- src/test/java/com/cydeo/tests/day12_pom_design_explicit_wait/DynamicLoad7Page.java
+++ src/test/java/com/cydeo/tests/day12_pom_design_explicit_wait/DynamicLoad7Page.java
@@ -1,4 +1,4 @@
-package com.cydeo.pages;
+package com.cydeo.tests.day12_pom_design_explicit_wait;
 
 import com.cydeo.utilities.Driver;
 import org.openqa.selenium.WebElement;
@@ -6,10 +6,11 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class DynamicLoad7Page {
-    //#1-Created constructor and instantiate driver and object of the class
+
+    //#1-Create constructor and instantiate driver and object of the class
 
     public DynamicLoad7Page(){
-        PageFactory.initElements(Driver.getDriver(),this);
+        PageFactory.initElements(Driver.getDriver(), this);
     }
 
     @FindBy(xpath = "//button[.='Start']")
@@ -30,6 +31,8 @@ public class DynamicLoad7Page {
 
     @FindBy(id = "flash")
     public WebElement errorMessage;
+
+
 
 
 }
