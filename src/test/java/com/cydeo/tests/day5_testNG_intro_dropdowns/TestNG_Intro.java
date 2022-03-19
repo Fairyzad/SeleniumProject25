df13bb9c7692b42cad751cac6d91ad8bdef202d1
peri <parizat@live.com>
2022-03-19 00:24:51 -0700

    TestNG_Intro

--- src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/TestNG_Intro.java
+++ src/test/java/com/cydeo/tests/day5_testNG_intro_dropdowns/TestNG_Intro.java
@@ -1,4 +1,20 @@
 package com.cydeo.tests.day5_testNG_intro_dropdowns;
 
+import org.testng.annotations.Test;
+
 public class TestNG_Intro {
+
+    @Test
+    public void test1(){
+        System.out.println("Test1 is running..");
+    }
+
+    @Test
+    public void test2(){
+
+        System.out.println("Test2 is running..");
+
+    }
+
+
 }
